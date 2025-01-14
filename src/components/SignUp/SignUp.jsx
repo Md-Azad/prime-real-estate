f05ac401d8444625f10be21a6fba3e01566e6d16
Md Azad Hossain <sokalazad@gmail.com>
2025-01-14 09:35:51 +0100

    added sign up page and it's functionality

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,99 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    const password = form.password.value;
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen ">
+      <div className="hero-content flex-col w-3/5  ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Sign Up now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full shadow-2xl ">
+          <form onSubmit={handleSignUp} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <input
+                className={`btn btn-primary `}
+                type="submit"
+                value="SignUp"
+              />
+            </div>
+          </form>
+          <div className="text-center">
+            <div className="divider divider-success">OR</div>
+
+            <h1>Google</h1>
+          </div>
+          <div className="divider "></div>
+          <p
+            className="text-center mb-4
+          "
+          >
+            <small>
+              Allready Have an Account?{" "}
+              <Link to="/login" className="text-yellow-500">
+                Login Here
+              </Link>
+            </small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/HomePage/Home/Home";
 import Login from "../components/Login/Login";
+import SignUp from "../components/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
       },
     ],
   },
