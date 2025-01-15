779e1fce5a3ff78c8b23dd0c41cf9638218cb8e9
Md Azad Hossain <sokalazad@gmail.com>
2025-01-15 01:34:04 +0100

    show all the added properties to admin

--- src/pages/DashboardPages/AdminPages/ManageProperties/ManageProperties.jsx
+++ src/pages/DashboardPages/AdminPages/ManageProperties/ManageProperties.jsx
@@ -15,7 +15,45 @@ const ManageProperties = () => {
   }
   return (
     <div>
-      <h1>Manage Properties. {properties.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Location</th>
+              <th>Agent Name</th>
+              <th>Agent Email</th>
+              <th>Price Range</th>
+              <th>Accept</th>
+              <th>Reject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {properties.map((property, index) => (
+              <tr key={property._id}>
+                <th>{index + 1}</th>
+                <td>{property?.title}</td>
+                <td>{property?.location}</td>
+                <td>{property?.name}</td>
+                <td>{property?.email}</td>
+                <td>
+                  {property?.min} - {property?.max}
+                </td>
+                <td>
+                  <button className="btn bg-green-700 text-white">
+                    Accept
+                  </button>
+                </td>
+                <td>
+                  <button className="btn bg-red-700 text-white">Reject</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
