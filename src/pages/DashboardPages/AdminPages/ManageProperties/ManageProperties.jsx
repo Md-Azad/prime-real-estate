eb6b0dd89540f249b41e9b0d8232d02f3517c1b0
Md Azad Hossain <sokalazad@gmail.com>
2025-01-15 01:22:43 +0100

    fetche all products

--- src/pages/DashboardPages/AdminPages/ManageProperties/ManageProperties.jsx
+++ src/pages/DashboardPages/AdminPages/ManageProperties/ManageProperties.jsx
@@ -1,7 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
 const ManageProperties = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: properties = [], isPending } = useQuery({
+    queryKey: ["property"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/products");
+      return res.data;
+    },
+  });
+  if (isPending) {
+    return <progress className="progress w-56"></progress>;
+  }
   return (
     <div>
-      <h1>Manage Properties</h1>
+      <h1>Manage Properties. {properties.length}</h1>
     </div>
   );
 };
