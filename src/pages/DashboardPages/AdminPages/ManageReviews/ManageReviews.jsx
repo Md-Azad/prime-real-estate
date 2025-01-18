337101c4340286ca5f8083b1af2577f84184eed1
Md Azad Hossain <sokalazad@gmail.com>
2025-01-19 00:21:49 +0100

    show all the reviews in manage review page

--- src/pages/DashboardPages/AdminPages/ManageReviews/ManageReviews.jsx
+++ src/pages/DashboardPages/AdminPages/ManageReviews/ManageReviews.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
+import { FaTrashAlt } from "react-icons/fa";
+
 const ManageReviews = () => {
   const axiosSecure = useAxiosSecure();
 
@@ -8,35 +10,59 @@ const ManageReviews = () => {
     queryKey: ["reviews"],
     queryFn: async () => {
       const res = await axiosSecure.get("/reviews");
-      console.log("reviews---------", res.data);
+
       return res.data;
     },
   });
+
   return (
     <div>
       <h1>Manage Reviews: {reviews.length}</h1>
-      <div className="overflow-x-auto">
-        <table className="table table-zebra">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {reviews.length > 0 ? (
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Reviewer name</th>
+                <th>email</th>
+                <th>Review</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reviews.map((review, idx) => (
+                <tr key={review._id}>
+                  <th>{idx + 1}</th>
+                  <td>
+                    {review?.reviewDetails[0]?.image ? (
+                      <img
+                        className="w-12 h-12 rounded-md"
+                        src={review.reviewDetails[0].image}
+                        alt=""
+                      />
+                    ) : (
+                      <p>N/A</p>
+                    )}
+                  </td>
+                  <td>{review?.reviewDetails[0].name}</td>
+                  <td>{review?.reviewerEmail}</td>
+                  <td>{review.review}</td>
+                  <td>
+                    <button className="btn btn-error">
+                      <FaTrashAlt className="text-white"></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h1> No Review Found</h1>
+      )}
     </div>
   );
 };
