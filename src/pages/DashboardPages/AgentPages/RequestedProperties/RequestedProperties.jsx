6a352b3c115b99d22adb98aaf12f930b1e751c79
Md Azad Hossain <sokalazad@gmail.com>
2025-01-20 03:12:53 +0100

    sold property shown in my sold property page

--- src/pages/DashboardPages/AgentPages/RequestedProperties/RequestedProperties.jsx
+++ src/pages/DashboardPages/AgentPages/RequestedProperties/RequestedProperties.jsx
@@ -2,21 +2,79 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
+import { ImCross } from "react-icons/im";
+
 const RequestedProperties = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: requestedList = [] } = useQuery({
+  const { data: requestedList = [], refetch } = useQuery({
     queryKey: ["requestedOffers", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/wishlist/${user?.email}`);
+      const res = await axiosSecure.get(`/myofferlist/${user?.email}`);
       return res.data;
     },
   });
   console.log(requestedList);
+  const handleAcceptOffer = (list) => {
+    const data = {
+      agentEmail: list?.agentEmail,
+      propertyId: list?.propertyId,
+      status: list?.status,
+      soldPrice: list?.offerPrice,
+    };
+    axiosSecure
+      .patch(`/acceptoffer/${list._id}`, data)
+      .then((res) => {
+        refetch();
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <div>
       <h1>Requested properties.{requestedList.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Property Title</th>
+              <th>Location</th>
+              <th>Buyer Name</th>
+              <th>Buyer Email</th>
+              <th>Offered Price</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {requestedList.map((list, idx) => (
+              <tr key={list._id}>
+                <th>{idx + 1}</th>
+                <td>{list?.propertyTitle} </td>
+                <td>{list?.propertyLocation}</td>
+                <td>{list?.buyerName}</td>
+                <td>{list?.buyerEmail}</td>
+                <td className="text-center">${list?.offerPrice}</td>
+                <td>
+                  <button
+                    onClick={() => handleAcceptOffer(list)}
+                    className="btn btn-success text-white"
+                  >
+                    Accept
+                  </button>
+                </td>
+                <td className=" btn btn-error">
+                  <ImCross className="text-white text-2xl"></ImCross>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/pages/DashboardPages/AgentPages/SoldProperties/SoldProperties.jsx
+++ src/pages/DashboardPages/AgentPages/SoldProperties/SoldProperties.jsx
@@ -1,7 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
 const SoldProperties = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: soldProperties = [] } = useQuery({
+    queryKey: ["sold", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/sold/${user?.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      <h1>my Sold Properties.</h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Property Title</th>
+              <th>Location</th>
+              <th>Buyer Name</th>
+              <th>Buyer Email</th>
+              <th>Sold Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {soldProperties.map((sold, idx) => (
+              <tr key={sold._id}>
+                <th>{idx + 1}</th>
+                <td>{sold?.propertyTitle}</td>
+                <td>{sold?.propertyLocation}</td>
+                <td>{sold?.buyerName}</td>
+                <td>{sold?.buyerEmail}</td>
+                <td>${sold?.offerPrice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
