122f16a63a46fb713b99938d9b54f97903b54868
Md Azad Hossain <sokalazad@gmail.com>
2025-01-23 00:14:33 +0100

    refactor all product code

--- src/pages/HomePage/AllProperties/AllProperties.jsx
+++ src/pages/HomePage/AllProperties/AllProperties.jsx
@@ -15,25 +15,56 @@ const AllProperties = () => {
       : property?.location.toLowerCase().includes(search);
   });
 
+  const [finalproperties, setFinalProperties] = useState(properties);
+
+  const handleMinSort = () => {
+    console.log("sorted");
+    const sortedPrice = allproperties.sort((a, b) => a.min - b.min);
+    setFinalProperties(sortedPrice);
+    console.log(finalproperties);
+  };
+
+  console.log(finalproperties);
+
   return (
     <div>
-      <div className="my-4 flex justify-end mr-12 items-center">
-        <label className="input input-bordered border-yellow-500 flex items-center gap-2">
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            className="grow"
-            placeholder="Search"
-          />
-          <kbd className="kbd kbd-sm">
-            <FaSearch />
-          </kbd>
-        </label>
+      <div className="flex flex-row-reverse justify-evenly items-center">
+        <div className="my-4 flex justify-center mr-12 items-center">
+          <label className="input input-bordered border-yellow-500 flex items-center gap-2">
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              className="grow"
+              placeholder="Search"
+            />
+            <kbd className="kbd kbd-sm">
+              <FaSearch />
+            </kbd>
+          </label>
+        </div>
+        <div className="flex justify-end gap-4 ">
+          <button onClick={handleMinSort} className="btn btn-info text-white">
+            Sort By Min Price
+          </button>
+          <button className="btn btn-accent text-white">
+            Sort By Max Price
+          </button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {allproperties.map((property) => (
+        {/* {finalproperties.map((property) => (
           <PropertyCard key={property._id} property={property}></PropertyCard>
-        ))}
+        ))} */}
+
+        {finalproperties
+          .filter((items) => {
+            return search === ""
+              ? items
+              : items?.location.toLowerCase().includes(search.toLowerCase());
+          })
+          .map((property) => (
+            <PropertyCard key={property._id} property={property}></PropertyCard>
+          ))}
       </div>
     </div>
   );
